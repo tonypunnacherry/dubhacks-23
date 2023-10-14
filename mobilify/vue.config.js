804be2ab6450d253c5ee680acca45795e321dd8a
Tony <tonytpunnacherry@gmail.com>
2023-10-14 12:44:48 -0700

    made vue project multipage

--- mobilify/vue.config.js
+++ mobilify/vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const pagesConfig = require("./src/entry/pages.config.js");
+//const { apiServer } = require("./src/api");
+//const notFoundHandler = require("./src/api/404.js");
+
+module.exports = {
+  pages: pagesConfig,
+  /*
+  devServer: {
+    historyApiFallback: false,
+    writeToDisk: true,
+    before: devServer => {
+      devServer.use(apiServer);
+    },
+    after: devServer => {
+      // if devServer hasn't responded to the request, we can assume no matches
+      devServer.use(notFoundHandler);
+    },
+  },
+  */
+};
